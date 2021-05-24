d3ef4c02f6b2475bdbca6b20c5a7395912d5e468
Suraj <64457728+Surajr37@users.noreply.github.com>
2021-05-24 13:54:18 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-1_JTi25cDVB',
-        userPoolClientId: '5qi4adsulhj6c35qgngict3car',
-        region: 'eu-west-1'
+        userPoolId: 'ap-south-1_Qt2x5IHns',
+        userPoolClientId: 'c69rffmt72tjg9j4gjosv8ovm',
+        region: 'ap-south-1'
     },
     api: {
         invokeUrl: 'https://k2cdp5ols3.execute-api.eu-west-1.amazonaws.com/prod'
