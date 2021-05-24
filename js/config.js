05f6e3bc600f8b4e487c25a431afa9bb0bea2ba9
Suraj <64457728+Surajr37@users.noreply.github.com>
2021-05-24 14:35:45 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,5 @@ window._config = {
         region: 'ap-south-1'
     },
     api: {
-        invokeUrl: 'https://g327ei4ref.execute-api.ap-south-1.amazonaws.com/prod'
+        invokeUrl: 'https://fihm9e4s49.execute-api.ap-south-1.amazonaws.com/prod'
     }
-};
